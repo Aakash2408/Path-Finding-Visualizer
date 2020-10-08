6b79f23a7d24b085724f163a2f78cdad5e3d141a
Aakash <aakashsangwan024@gmail.com>
2020-10-08 15:53:49 +0530

    Adding algos

---
+++ node_modules/@material-ui/core/esm/locale/index.js
@@ -0,0 +1,446 @@
+export var deDE = {
+  props: {
+    MuiTablePagination: {
+      backIconButtonText: 'Nächste Seite',
+      labelRowsPerPage: 'Zeilen pro Seite:',
+      labelDisplayedRows: function labelDisplayedRows(_ref) {
+        var from = _ref.from,
+            to = _ref.to,
+            count = _ref.count;
+        return "".concat(from, "-").concat(to === -1 ? count : to, " von ").concat(count);
+      },
+      nextIconButtonText: 'Nächste Seite'
+    },
+    MuiRating: {
+      getLabelText: function getLabelText(value) {
+        return "".concat(value, " ").concat(value !== 1 ? 'Sterne' : 'Stern');
+      }
+    },
+    MuiAutocomplete: {
+      clearText: 'Leeren',
+      closeText: 'Schließen',
+      loadingText: 'Wird geladen…',
+      noOptionsText: 'Keine Optionen',
+      openText: 'Öffnen'
+    }
+  }
+}; // default
+
+export var enUS = {};
+/**
+  props: {
+    MuiTablePagination: {
+      backIconButtonText: 'Previous page',
+      labelRowsPerPage: 'Rows per page:',
+      labelDisplayedRows: ({ from, to, count }) => `${from}-${to === -1 ? count : to} of ${count}`,
+      nextIconButtonText: 'Next page',
+    },
+    MuiRating: {
+      getLabelText: value => `${value} Star${value !== 1 ? 's' : ''}`,
+    },
+    MuiAutocomplete: {
+      clearText: 'Clear',
+      closeText: 'Close',
+      loadingText: 'Loading…',
+      noOptionsText: 'No options',
+      openText: 'Open',
+    },
+  },
+*/
+
+export var esES = {
+  props: {
+    MuiTablePagination: {
+      backIconButtonText: 'Página anterior',
+      labelRowsPerPage: 'Filas por página:',
+      labelDisplayedRows: function labelDisplayedRows(_ref2) {
+        var from = _ref2.from,
+            to = _ref2.to,
+            count = _ref2.count;
+        return "".concat(from, "-").concat(to === -1 ? count : to, " de ").concat(count);
+      },
+      nextIconButtonText: 'Siguiente página'
+    },
+    MuiRating: {
+      getLabelText: function getLabelText(value) {
+        return "".concat(value, " Estrella").concat(value !== 1 ? 's' : '');
+      }
+    },
+    MuiAutocomplete: {
+      clearText: 'Limpiar',
+      closeText: 'Cerrar',
+      loadingText: 'Cargando…',
+      noOptionsText: 'Sin opciones',
+      openText: 'Abierto'
+    }
+  }
+};
+export var faIR = {
+  props: {
+    MuiTablePagination: {
+      backIconButtonText: 'صفحهٔ قبل',
+      labelRowsPerPage: 'تعداد سطرهای هر صفحه:',
+      labelDisplayedRows: function labelDisplayedRows(_ref3) {
+        var from = _ref3.from,
+            to = _ref3.to,
+            count = _ref3.count;
+        return "".concat(from, "-").concat(to === -1 ? count : to, " \u0627\u0632 ").concat(count);
+      },
+      nextIconButtonText: 'صفحهٔ بعد'
+    },
+    MuiRating: {
+      getLabelText: function getLabelText(value) {
+        return "".concat(value, " \u0633\u062A\u0627\u0631\u0647");
+      }
+    },
+    MuiAutocomplete: {
+      clearText: 'پاک‌کردن',
+      closeText: 'بستن',
+      loadingText: 'در حال بارگذاری…',
+      noOptionsText: 'بی‌نتیجه',
+      openText: 'بازکردن'
+    }
+  }
+};
+export var frFR = {
+  props: {
+    MuiTablePagination: {
+      backIconButtonText: 'Page précédente',
+      labelRowsPerPage: 'Lignes par page :',
+      labelDisplayedRows: function labelDisplayedRows(_ref4) {
+        var from = _ref4.from,
+            to = _ref4.to,
+            count = _ref4.count;
+        return "".concat(from, "-").concat(to === -1 ? count : to, " sur ").concat(count);
+      },
+      nextIconButtonText: 'Page suivante'
+    },
+    MuiRating: {
+      getLabelText: function getLabelText(value) {
+        return "".concat(value, " Etoile").concat(value !== 1 ? 's' : '');
+      }
+    },
+    MuiAutocomplete: {
+      clearText: 'Vider',
+      closeText: 'Fermer',
+      loadingText: 'Chargement…',
+      noOptionsText: 'Pas de résultats',
+      openText: 'Ouvrir'
+    }
+  }
+};
+export var idID = {
+  props: {
+    MuiTablePagination: {
+      backIconButtonText: 'Halaman sebelumnya',
+      labelRowsPerPage: 'Baris per halaman:',
+      labelDisplayedRows: function labelDisplayedRows(_ref5) {
+        var from = _ref5.from,
+            to = _ref5.to,
+            count = _ref5.count;
+        return "".concat(from, "-").concat(to === -1 ? count : to, " dari ").concat(count);
+      },
+      nextIconButtonText: 'Halaman selanjutnya'
+    },
+    MuiRating: {
+      getLabelText: function getLabelText(value) {
+        return "".concat(value, " Bintang");
+      }
+    },
+    MuiAutocomplete: {
+      clearText: 'Hapus',
+      closeText: 'Tutup',
+      loadingText: 'Memuat…',
+      noOptionsText: 'Tidak ada opsi',
+      openText: 'Buka'
+    }
+  }
+};
+export var itIT = {
+  props: {
+    MuiTablePagination: {
+      backIconButtonText: 'Pagina precedente',
+      labelRowsPerPage: 'Righe per pagina:',
+      labelDisplayedRows: function labelDisplayedRows(_ref6) {
+        var from = _ref6.from,
+            to = _ref6.to,
+            count = _ref6.count;
+        return "".concat(from, "-").concat(to === -1 ? count : to, " di ").concat(count);
+      },
+      nextIconButtonText: 'Pagina successiva'
+    },
+    MuiRating: {
+      getLabelText: function getLabelText(value) {
+        return "".concat(value, " Stell").concat(value !== 1 ? 'a' : 'e');
+      }
+    },
+    MuiAutocomplete: {
+      clearText: 'Svuota',
+      closeText: 'Chiudi',
+      loadingText: 'Caricamento in corso…',
+      noOptionsText: 'Nessuna opzione',
+      openText: 'Apri'
+    }
+  }
+};
+export var jaJP = {
+  props: {
+    MuiTablePagination: {
+      backIconButtonText: '前のページ',
+      labelRowsPerPage: 'ページごとの行:',
+      labelDisplayedRows: function labelDisplayedRows(_ref7) {
+        var from = _ref7.from,
+            to = _ref7.to,
+            count = _ref7.count;
+        return "".concat(from, "-").concat(to === -1 ? count : to, " of ").concat(count);
+      },
+      nextIconButtonText: '次のページ'
+    },
+    MuiRating: {
+      getLabelText: function getLabelText(value) {
+        return "".concat(value, " ").concat(value !== 1 ? '出演者' : '星');
+      }
+    },
+    MuiAutocomplete: {
+      clearText: 'クリア',
+      closeText: '閉じる',
+      loadingText: '積み込み…',
+      noOptionsText: '結果がありません',
+      openText: '開いた'
+    }
+  }
+};
+export var nlNL = {
+  props: {
+    MuiTablePagination: {
+      backIconButtonText: 'Vorige pagina',
+      labelRowsPerPage: 'Regels per pagina :',
+      labelDisplayedRows: function labelDisplayedRows(_ref8) {
+        var from = _ref8.from,
+            to = _ref8.to,
+            count = _ref8.count;
+        return "".concat(from, "-").concat(to === -1 ? count : to, " van ").concat(count);
+      },
+      nextIconButtonText: 'Volgende pagina'
+    },
+    MuiRating: {
+      getLabelText: function getLabelText(value) {
+        return "".concat(value, " Ster").concat(value !== 1 ? 'ren' : '');
+      }
+    },
+    MuiAutocomplete: {
+      clearText: 'Wissen',
+      closeText: 'Sluiten',
+      loadingText: 'Laden…',
+      noOptionsText: 'Geen opties',
+      openText: 'Openen'
+    }
+  }
+};
+export var plPL = {
+  props: {
+    MuiTablePagination: {
+      backIconButtonText: 'Poprzednia strona',
+      labelRowsPerPage: 'Wierszy na stronę:',
+      labelDisplayedRows: function labelDisplayedRows(_ref9) {
+        var from = _ref9.from,
+            to = _ref9.to,
+            count = _ref9.count;
+        return "".concat(from, "-").concat(to === -1 ? count : to, " z ").concat(count);
+      },
+      nextIconButtonText: 'Następna strona'
+    },
+    MuiRating: {
+      getLabelText: function getLabelText(value) {
+        var pluralForm = 'gwiazdek';
+        var lastDigit = value % 10;
+
+        if ((value < 10 || value > 20) && lastDigit > 1 && lastDigit < 5) {
+          pluralForm = 'gwiazdki';
+        } else if (value === 1) {
+          pluralForm = 'gwiazdka';
+        }
+
+        return "".concat(value, " ").concat(pluralForm);
+      }
+    },
+    MuiAutocomplete: {
+      clearText: 'Wyczyść',
+      closeText: 'Zamknij',
+      loadingText: 'Ładowanie…',
+      noOptionsText: 'Brak opcji',
+      openText: 'Otwórz'
+    }
+  }
+};
+export var ptBR = {
+  props: {
+    MuiTablePagination: {
+      backIconButtonText: 'Página anterior',
+      labelRowsPerPage: 'Linhas por página:',
+      labelDisplayedRows: function labelDisplayedRows(_ref10) {
+        var from = _ref10.from,
+            to = _ref10.to,
+            count = _ref10.count;
+        return "".concat(from, "-").concat(to === -1 ? count : to, " de ").concat(count);
+      },
+      nextIconButtonText: 'Próxima página'
+    },
+    MuiRating: {
+      getLabelText: function getLabelText(value) {
+        return "".concat(value, " Estrela").concat(value !== 1 ? 's' : '');
+      }
+    },
+    MuiAutocomplete: {
+      clearText: 'Limpar',
+      closeText: 'Fechar',
+      loadingText: 'Carregando…',
+      noOptionsText: 'Sem opções',
+      openText: 'Abrir'
+    }
+  }
+};
+export var roRO = {
+  props: {
+    MuiTablePagination: {
+      backIconButtonText: 'Pagina precedentă',
+      labelRowsPerPage: 'Rânduri pe pagină:',
+      labelDisplayedRows: function labelDisplayedRows(_ref11) {
+        var from = _ref11.from,
+            to = _ref11.to,
+            count = _ref11.count;
+        return "".concat(from, "-").concat(to === -1 ? count : to, " din ").concat(count);
+      },
+      nextIconButtonText: 'Pagina următoare'
+    },
+    MuiRating: {
+      getLabelText: function getLabelText(value) {
+        return "".concat(value, " St").concat(value !== 1 ? 'ele' : 'ea');
+      }
+    },
+    MuiAutocomplete: {
+      clearText: 'Șterge',
+      closeText: 'Închide',
+      loadingText: 'Se încarcă…',
+      noOptionsText: 'Nicio opțiune',
+      openText: 'Deschide'
+    }
+  }
+};
+export var ruRU = {
+  props: {
+    MuiTablePagination: {
+      backIconButtonText: 'Предыдущая страница',
+      labelRowsPerPage: 'Строк на странице:',
+      labelDisplayedRows: function labelDisplayedRows(_ref12) {
+        var from = _ref12.from,
+            to = _ref12.to,
+            count = _ref12.count;
+        return "".concat(from, "-").concat(to === -1 ? count : to, " \u0438\u0437 ").concat(count);
+      },
+      nextIconButtonText: 'Следующая страница'
+    },
+    MuiRating: {
+      getLabelText: function getLabelText(value) {
+        var pluralForm = 'Звёзд';
+        var lastDigit = value % 10;
+
+        if (lastDigit > 1 && lastDigit < 5) {
+          pluralForm = 'Звезды';
+        } else if (lastDigit === 1) {
+          pluralForm = 'Звезда';
+        }
+
+        return "".concat(value, " ").concat(pluralForm);
+      }
+    },
+    MuiAutocomplete: {
+      clearText: 'Очистить',
+      closeText: 'Закрыть',
+      loadingText: 'Загрузка…',
+      noOptionsText: 'Нет доступных вариантов',
+      openText: 'Открыть'
+    }
+  }
+};
+export var svSE = {
+  props: {
+    MuiTablePagination: {
+      backIconButtonText: 'Föregående sida',
+      labelRowsPerPage: 'Rader per sida:',
+      labelDisplayedRows: function labelDisplayedRows(_ref13) {
+        var from = _ref13.from,
+            to = _ref13.to,
+            count = _ref13.count;
+        return "".concat(from, "-").concat(to === -1 ? count : to, " av ").concat(count);
+      },
+      nextIconButtonText: 'Nästa sida'
+    },
+    MuiRating: {
+      getLabelText: function getLabelText(value) {
+        return "".concat(value, " ").concat(value !== 1 ? 'Stjärnor' : 'Stjärna');
+      }
+    },
+    MuiAutocomplete: {
+      clearText: 'Rensa',
+      closeText: 'Stäng',
+      loadingText: 'Laddar…',
+      noOptionsText: 'Inga alternativ',
+      openText: 'Öppen'
+    }
+  }
+};
+export var trTR = {
+  props: {
+    MuiTablePagination: {
+      backIconButtonText: 'Önceki sayfa',
+      labelRowsPerPage: 'Sayfa başına satır:',
+      labelDisplayedRows: function labelDisplayedRows(_ref14) {
+        var from = _ref14.from,
+            to = _ref14.to,
+            count = _ref14.count;
+        return "".concat(count, " tanesinden ").concat(from, "-").concat(to === -1 ? count : to);
+      },
+      nextIconButtonText: 'Sonraki sayfa'
+    },
+    MuiRating: {
+      getLabelText: function getLabelText(value) {
+        return "".concat(value, " Y\u0131ld\u0131z");
+      }
+    },
+    MuiAutocomplete: {
+      clearText: 'Temizle',
+      closeText: 'Kapat',
+      loadingText: 'Yükleniyor…',
+      noOptionsText: 'Seçenek yok',
+      openText: 'Aç'
+    }
+  }
+};
+export var zhCN = {
+  props: {
+    MuiTablePagination: {
+      backIconButtonText: '上一页',
+      labelRowsPerPage: '每页行数:',
+      labelDisplayedRows: function labelDisplayedRows(_ref15) {
+        var from = _ref15.from,
+            to = _ref15.to,
+            count = _ref15.count;
+        return "".concat(from, "-").concat(to === -1 ? count : to, " \u7684 ").concat(count);
+      },
+      nextIconButtonText: '下一页'
+    },
+    MuiRating: {
+      getLabelText: function getLabelText(value) {
+        return "".concat(value, " \u661F").concat(value !== 1 ? '星' : '');
+      }
+    },
+    MuiAutocomplete: {
+      clearText: '明确',
+      closeText: '关',
+      loadingText: '载入中…',
+      noOptionsText: '没有选择',
+      openText: '打开'
+    }
+  }
+};
---
+++ node_modules/@types/testing-library__dom/README.md
@@ -0,0 +1,16 @@
+# Installation
+> `npm install --save @types/testing-library__dom`
+
+# Summary
+This package contains type definitions for @testing-library/dom (https://github.com/testing-library/dom-testing-library).
+
+# Details
+Files were exported from https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/testing-library__dom
+
+Additional Details
+ * Last updated: Fri, 01 Nov 2019 15:46:20 GMT
+ * Dependencies: @types/pretty-format
+ * Global values: none
+
+# Credits
+These definitions were written by Alex Krolick <https://github.com/alexkrolick>, Kent C Dodds <https://github.com/kentcdodds>, Sebastian Silbermann <https://github.com/eps1lon>, Weyert de Boer <https://github.com/weyert>, and Ronald Rey <https://github.com/reyronald>.
